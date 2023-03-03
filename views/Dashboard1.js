b3887f1544c95029c4c53832f5c4e9266ff69dcf
dnieto1997 <daironieto6000@hotmail.com>
2023-03-03 09:57:18 -0500

    actualizacion

--- views/Dashboard1.js
+++ views/Dashboard1.js
@@ -1,6 +1,7 @@
 
 import 'react-native-gesture-handler';
 import React,{useEffect,useState} from 'react'
+import { formatearCantidad } from '../helpers/Index';
 
 import {
     SafeAreaView,
@@ -18,6 +19,10 @@ import {
  const Dashboard1 = () => {
     const[token,setToken] =useState('')
     const [todaymoneyin,setTodaymoneyin] =useState('')
+    const [todaystransactionin,settodaystransactionin] =useState('')
+    const [todaystransactionerrorin,setttodaystransactionerrorin] =useState('')
+    const [todaymoneyout,settodaymoneyout] =useState('')
+    const [todaystransactionout,settodaystransactionout] =useState('')
    
     useEffect(()=>{
         const obtenerToken =async () =>{
@@ -40,8 +45,12 @@ import {
         useEffect(()=>{
 
             todayMoneyin()
+            todaysTransactionin()
+            todaysTransactionerrorin()
+            todayMoneyout()
+            todaysTransactionout()
             
-            },[token]) 
+            }) 
         
           const  todayMoneyin = async () => {
          
@@ -67,8 +76,108 @@ import {
             
             
             }
+
+            const  todaysTransactionin = async () => {
+         
+              try {
+                  
+             const res = await fetch('http://129.80.238.214:3000/api/dashboard/todaystransactionin', {
+              method: 'GET',
+              headers: {
+                
+                  'x-token':`${token}`
+              }
+            });
+          
+                  const { result } = await res.json();
+                  
+                  const total = result[0].total;
+                  settodaystransactionin(total)
+                  
+          
+                } catch (err) {
+                  console.log(err);
+                }
+              
+              
+              }
+
+
+              const  todaysTransactionerrorin = async () => {
+         
+                try {
+                    
+               const res = await fetch('http://129.80.238.214:3000/api/dashboard/todaystransactionerrorin', {
+                method: 'GET',
+                headers: {
+                  
+                    'x-token':`${token}`
+                }
+              });
+            
+                    const { result } = await res.json();
+                    
+                    const total = result[0].total;
+                    setttodaystransactionerrorin(total)
+                    
+            
+                  } catch (err) {
+                    console.log(err);
+                  }
+                
+                
+                }
+
+                const  todayMoneyout = async () => {
+         
+                  try {
+                      
+                 const res = await fetch('http://129.80.238.214:3000/api/dashboard/todaymoneyout', {
+                  method: 'GET',
+                  headers: {
+                    
+                      'x-token':`${token}`
+                  }
+                });
+              
+                      const { result } = await res.json();
+                      
+                      const total = result[0].total;
+                      settodaymoneyout(total)
+                      
+              
+                    } catch (err) {
+                      console.log(err);
+                    }
+                  
+                  
+                  }
              
 
+                  const  todaysTransactionout = async () => {
+         
+                    try {
+                        
+                   const res = await fetch('http://129.80.238.214:3000/api/dashboard/todaystransactionout', {
+                    method: 'GET',
+                    headers: {
+                      
+                        'x-token':`${token}`
+                    }
+                  });
+                
+                        const { result } = await res.json();
+                        
+                        const total = result[0].total;
+                        settodaystransactionout(total)
+                        
+                
+                      } catch (err) {
+                        console.log(err);
+                      }
+                    
+                    
+                    }
           
 
              
@@ -85,12 +194,46 @@ import {
     </View>
      <View style={{marginLeft:10}}>
       <Text style={styles.texto1}>Today's Money Cash in Success</Text>
-      <Text >{todaymoneyin}</Text>
+      <Text style={styles.texto2} >{formatearCantidad(todaymoneyin)} </Text>
       </View>
      </View>
      </View>
      </View>
 
+
+     
+     <View style={styles.contenedor}>
+      <View style={globalStyles.contenedor2}>
+     <View style={styles.contenedor2} >
+     <View >
+    <Image source={require('../assets/img/cash.png')} style={styles.imagen}/>
+    </View>
+     <View style={{marginLeft:10}}>
+      <Text style={styles.texto1}>Today's Transaction Cash In</Text>
+      <Text style={styles.texto2}>{todaystransactionin} (COP) <Text style={{color:'green'}}>Success</Text></Text>
+      <Text style={styles.texto2}>{todaystransactionerrorin} (COP) <Text style={{color:'red'}}>Pending</Text></Text>
+      </View>
+     </View>
+     </View>
+     </View>
+
+
+     <View style={styles.contenedor}>
+      <View style={globalStyles.contenedor2}>
+     <View style={styles.contenedor2} >
+     <View >
+    <Image source={require('../assets/img/cash2.png')} style={styles.imagen}/>
+    </View>
+     <View style={{marginLeft:10}}>
+    
+    <Text style={[styles.texto1,styles.texto0]}>Today's Money Transaction Cash Out</Text>
+   
+      
+      <Text style={styles.texto2} >{formatearCantidad(todaymoneyout)} </Text>
+      </View>
+     </View>
+     </View>
+     </View>
 
      <View style={styles.contenedor}>
       <View style={globalStyles.contenedor2}>
@@ -100,36 +243,7 @@ import {
     </View>
      <View style={{marginLeft:10}}>
       <Text style={styles.texto1}>Today's Transaction Cash Out</Text>
-      <Text>{token} </Text>
-      </View>
-     </View>
-     </View>
-     </View>
-
-     <View style={styles.contenedor}>
-      <View style={globalStyles.contenedor2}>
-     <View style={styles.contenedor2} >
-     <View >
-    <Image source={require('../assets/img/cash.png')} style={styles.imagen}/>
-    </View>
-     <View style={{marginLeft:10}}>
-      <Text style={styles.texto1}>Today's Transaction Cash In</Text>
-      <Text>454 (COP) Success API</Text>
-      <Text>1,811 (COP) Pending</Text>
-      </View>
-     </View>
-     </View>
-     </View>
-
-     <View style={styles.contenedor}>
-      <View style={globalStyles.contenedor2}>
-     <View style={styles.contenedor2} >
-     <View >
-    <Image source={require('../assets/img/cash2.png')} style={styles.imagen}/>
-    </View>
-     <View style={{marginLeft:10}}>
-      <Text style={styles.texto1}>Today's Transaction Cash Out</Text>
-      <Text>consulta de api  </Text>
+      <Text style={styles.texto2}>{todaystransactionout} (COP) <Text style={{color:'green'}}>Success</Text></Text>
       
       </View>
      </View>
@@ -151,7 +265,19 @@ const styles = StyleSheet.create({
     },contenedor2:{
       flexDirection:'row'
     },texto1:{
-      marginBottom:40
+      marginBottom:40,
+     
+      fontSize:12,
+      textTransform:'uppercase',
+     fontWeight:'bold'
+    },texto2:{
+      textAlign:'center',
+      fontSize:19,
+      textTransform:'uppercase',
+     fontWeight:'bold'
+    },texto0:{
+      fontSize:10
+      
     }
   })
 export default Dashboard1
