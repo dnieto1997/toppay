8c98f987f8a1f05dbee167d35ba4d8e6693a4914
dnieto1997 <daironieto6000@hotmail.com>
2023-03-07 14:58:52 -0500

    actualizaer

--- nav/Tab.js
+++ nav/Tab.js
@@ -1,26 +1,27 @@
+import 'react-native-gesture-handler';
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 
 
 import Payout from '../views/Payout'
 import { NavigationContainer } from '@react-navigation/native'
 import Payin from '../views/src/Payin'
 import Utilidades from '../views/src/Utilidades'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import Dashboard from '../views/Dashboard';
 
 
 const Tab = () => {
- const Tab= createBottomTabNavigator()
+  const Drawer = createDrawerNavigator();
  
  return (
     
-    <NavigationContainer independent={true}>
-    <Tab.Navigator>
-    <Tab.Screen name="Das" component={Payout}/>
-    <Tab.Screen name="Payout" component={Payout}/>
-    <Tab.Screen name="Payin" component={Payin}/>
-    <Tab.Screen name="Utilidades" component={Utilidades}/>
-    </Tab.Navigator>
-    </NavigationContainer>
+  <Drawer.Navigator>
+    <Drawer.Screen name="Dashboard " component={Dashboard} />
+  <Drawer.Screen name="Payout" component={Payout} />
+  <Drawer.Screen name="Payin" component={Payin} />
+  <Drawer.Screen name="Utilidades" component={Utilidades} />
+</Drawer.Navigator>
   )
 }
 
